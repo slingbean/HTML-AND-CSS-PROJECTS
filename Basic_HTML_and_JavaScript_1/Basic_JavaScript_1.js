9ad3cc9b7c280a1d47f645f10d3d3f5e6f5ffae2
Cara Slingsby <cara.slingsby@gmail.com>
2026-09-23 12:38:13 -0700

    Changed locations of folders and added Assignment for submission

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,9 @@
+
+
+
+    function My_First_Function() {
+        var string= "Kiss me, I'm Irish!";
+        var result= string.fontcolor("green");
+        document.getElementById("Irish").innerHTML =
+        result;
+    }
