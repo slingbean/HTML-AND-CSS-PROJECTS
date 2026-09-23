c42b7ac5b51f8f309654602b605c562a76e567f0
Cara Slingsby <cara.slingsby@gmail.com>
2026-09-23 12:39:40 -0700

    Delete Basic_HTML_and_JavaScript_1 directory

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++
@@ -1,9 +0,0 @@
-
-
-
-    function My_First_Function() {
-        var string= "Kiss me, I'm Irish!";
-        var result= string.fontcolor("green");
-        document.getElementById("Irish").innerHTML =
-        result;
-    }
